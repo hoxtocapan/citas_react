054eb4d2901e121baf80898f19891269a2f6014e
hoxtocapan <Hoxtocapan@gmail.com>
2023-11-10 22:44:52 -0600

    Modificacion de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
-        Seguimiento Paciente {''}
+        Seguimiento Paciente {''} Con Github {''}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
